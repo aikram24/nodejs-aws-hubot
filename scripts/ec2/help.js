b09109f3aaecdae4f034e9c6387f47de3a0f5f5e
aikram24 <aikram24@hotmail.com>
2016-09-15 15:22:43 -0400

    adding help menu for ami command

--- scripts/ec2/help.js
+++ scripts/ec2/help.js
@@ -1,8 +1,11 @@
 module.exports = function(robot) {
   return robot.respond(/ec2 help(.*)$/i, function(msg) {
     var helpMessage = "*EC2 Help Menu:* \n"
-    helpMessage += "*--instance_id=* \*\*\*        : [optional] The id of an instance. If omit it, returns info about all instances. \n"
-    helpMessage += "*--instance_filter=* \*\*\*    : [optional] The name of an instance. If omit it, returns info about all instance \n"
+    helpMessage += "*ls* \*\*\*        		      : Returns info about all instances. \n"
+    helpMessage += "*ls --instance_id=* \*\*\*        : [optional] The id of an instance. If omit it, returns info about all instances. \n"
+    helpMessage += "*ls --instance_filter=* \*\*\*    : [optional] The name of an instance. If omit it, returns info about all instance \n"
+    helpMessage += "*ami ls* \*\*\*    		      : Returns info about all AMIs \n"
+    helpMessage += "*ami ls ----owner=* \*\*\*        : [optional] The owner of an AMI. If omit it, returns info about all AMIs. \n"
   return msg.send(helpMessage);
 
   });
